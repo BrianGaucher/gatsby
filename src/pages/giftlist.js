28569746d430cc12ea2ea9441070d6e2b1550d3b
Phineas Gaucher <brianphineasgaucher@gmail.com>
2025-11-13 17:32:15 -0500

    Update giftlist.js
    Fixed heading

--- src/pages/giftlist.js
+++ src/pages/giftlist.js
@@ -8,10 +8,8 @@ const GiftListPage = () => {
       <nav style={{ marginBottom: "1rem", fontSize: "0.9rem" }}>
         <Link to="/" style={{ textDecoration: "none", color: "#0070f3" }}>Home</Link>
         <span style={{ margin: "0 0.5rem" }}>→</span>
+        <span style={{ color: "#555" }}>Gift List</span>
       </nav>
-
-      {/* Page header */}
-      <h1 style={{ marginBottom: "1rem" }}>Gift List</h1>
 
 {/* Embedded Etherpad */}
 <div
