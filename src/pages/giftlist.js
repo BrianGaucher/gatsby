8f4d176f15099727bffd6b961706f6d9d18e6111
Phineas Gaucher <brianphineasgaucher@gmail.com>
2025-11-13 16:54:59 -0500

    Update giftlist.js

--- src/pages/giftlist.js
+++ src/pages/giftlist.js
@@ -2,10 +2,21 @@ import React from "react";
 
 const GiftListPage = () => {
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <div
+      style={{
+        maxWidth: 600,
+        margin: "2rem auto",
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+        overflow: "hidden",
+        boxShadow: "0 2px 6px rgba(0,0,0,0.1)",
+      }}
+    >
       <iframe
-        src="https://demo.hedgedoc.org/s/meZyNSlQU#"
-        style={{ width: "100%", height: "100%", border: "none" }}
+        src="https://u.cave.su/p/u98j75-SuaWDg5QwBZp1?showControls=false&showChat=true&showLineNumbers=true&useMonospaceFont=false"
+        width="100%"
+        height="600"
+        style={{ border: "none" }}
         title="Gift List"
       ></iframe>
     </div>
