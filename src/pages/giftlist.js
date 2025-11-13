63d796a95fa028942b4b3a773ce03baeb8398bc7
Phineas Gaucher <brianphineasgaucher@gmail.com>
2025-11-13 16:05:33 -0500

    Create giftlist.js
    Adding gift registry

---
+++ src/pages/giftlist.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const GiftListPage = () => {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://demo.hedgedoc.org/s/meZyNSlQU#"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        title="Gift List"
+      ></iframe>
+    </div>
+  );
+};
+
+export default GiftListPage;
